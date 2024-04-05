ed01b860023d18fc87ade95fcd85fb82d6108ba5
Фролова Екатерина Сергеевна <20047@iat.iat>
2024-04-05 13:58:32 +0800

    Решение задач на с#

---
+++ tasks1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Customer
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public DateTime DateAdded { get; set; }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Customer> customers = new List<Customer>();
+
+        while (true)
+        {
+            Console.WriteLine("Введите информацию о клиенте(ID, Name, Address, DateAdded):");
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "Выход")
+            {
+                break;
+            }
+
+            string[] values = input.Split(',');
+
+            if (values.Length != 4)
+            {
+                Console.WriteLine("Неверный Ввод. Пожалуйста, попробуйте еще раз.");
+                continue;
+            }
+
+            Customer customer = new Customer
+            {
+                Id = int.Parse(values[0]),
+                Name = values[1],
+                Address = values[2],
+                DateAdded = DateTime.Parse(values[3])
+            };
+
+            customers.Add(customer);
+            customers.Sort((x, y) => x.DateAdded.CompareTo(y.DateAdded));
+
+            Console.WriteLine("Клиент добавлен в список.");
+        }
+
+        Console.WriteLine("Список клиентов в порядке очереди: ");
+        foreach (Customer customer in customers)
+        {
+            Console.WriteLine($"{customer.Id}, {customer.Name}, {customer.Address}, {customer.DateAdded}");
+        }
+    }
+}
---
+++ tasks2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите имя файла: ");
+        string fileName = Console.ReadLine();
+
+        Console.Write("Введите количество строк (0 < N < 27): ");
+        int N = int.Parse(Console.ReadLine());
+
+        if (N < 1 || N >= 27)
+        {
+            Console.WriteLine("Недопустимое количество строк.");
+            return;
+        }
+
+        using (StreamWriter writer = new StreamWriter(fileName))
+        {
+            for (int i = 1; i <= N; i++)
+            {
+                string line = new string((char)('a' + i - 1), i);
+                writer.WriteLine(line);
+            }
+        }
+
+        Console.WriteLine("Файл создан и успешно записан.");
+    }
+}
